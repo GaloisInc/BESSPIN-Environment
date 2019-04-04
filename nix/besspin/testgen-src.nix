712cb43583d7828d09082002ace2dc5bd59eb60f
Stuart Pernsteiner <spernsteiner@galois.com>
2019-04-04 14:54:54 -0700

    bump bofgen version

--- nix/besspin/testgen-src.nix
+++ nix/besspin/testgen-src.nix
@@ -2,6 +2,6 @@
 
 builtins.fetchGit {
   url = "git@gitlab-ext.galois.com:ssith/testgen.git";
-  rev = "af43f045504a8d956c8182130a80695e2feabca7";
+  rev = "5c1668551fc47db28bebcbe32c80fd1560ec0c2d";
   ref = "nix-prep";
 }
