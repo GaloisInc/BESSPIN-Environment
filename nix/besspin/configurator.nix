6cee338faa693bb391b5a5b6acdd4537bc02b831
Stuart Pernsteiner <spernsteiner@galois.com>
2019-04-08 14:57:06 -0700

    bump configurator version

--- nix/besspin/configurator.nix
+++ nix/besspin/configurator.nix
@@ -5,7 +5,7 @@ srcOnly {
 
   src = builtins.fetchGit {
     url = "git@gitlab-ext.galois.com:ssith/feature-model-configurator-ui.git";
-    rev = "b53f6875b0583f6d55e33ffedcecf373d196818d";
-    ref = "nix-paths";
+    rev = "e31fb6060d323f1dae5a3813688e20b478ab17aa";
+    ref = "fix-multi-top-level-features";
   };
 }
