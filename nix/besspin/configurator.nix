a5029324ad56a26aa35fed37112064be4df0c8e1
Stuart Pernsteiner <spernsteiner@galois.com>
2019-04-08 18:26:02 -0700

    bump configurator and testgen revs

--- nix/besspin/configurator.nix
+++ nix/besspin/configurator.nix
@@ -5,7 +5,6 @@ srcOnly {
 
   src = builtins.fetchGit {
     url = "git@gitlab-ext.galois.com:ssith/feature-model-configurator-ui.git";
-    rev = "e31fb6060d323f1dae5a3813688e20b478ab17aa";
-    ref = "fix-multi-top-level-features";
+    rev = "fbb361ba5ecf9cd1a3e0d2167f61071d6a205378";
   };
 }
