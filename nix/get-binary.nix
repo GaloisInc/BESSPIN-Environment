d1831018e290050f7f6103ffe6df00b723c8e63a
Stuart Pernsteiner <spernsteiner@galois.com>
2019-04-09 11:42:31 -0700

    change handling of binary packages

--- nix/get-binary.nix
+++ nix/get-binary.nix
@@ -6,12 +6,12 @@ stdenv.mkDerivation rec {
   name = orig.name;
   src = builtins.fetchGit {
     url = "git@gitlab-ext.galois.com:ssith/tool-suite-binaries.git";
-    rev = "7bc0e0ce9c5546aa9749c3f0c92b80d7a6e3e4ed";
+    rev = "263249d6835d8c58a0d18f422d71318a5392ea58";
   };
 
   phases = [ "unpackPhase" "installPhase" ];
 
   installPhase = ''
-    cp -r ${baseNameOf orig}/ $out/
+    cp -r ${orig.name}/ $out/
   '';
 }
