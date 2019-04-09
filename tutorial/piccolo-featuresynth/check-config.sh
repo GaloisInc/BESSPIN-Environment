d62f007d5149667a9244260c19b7d884f7ecc30a
Stuart Pernsteiner <spernsteiner@galois.com>
2019-04-08 18:19:15 -0700

    add piccolo featuresynth helper files

---
+++ tutorial/piccolo-featuresynth/check-config.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+work=$(dirname "$0")
+inst=$1
+
+mkdir -p $work/logs
+exec 2>$work/logs/$(date +%Y%m%d-%H%M%S)-$$-$inst.log 1>&2
+
+piccolo=$PWD/../../../Piccolo
+
+inst_dir=$work/$inst
+
+if [ ! -d $inst_dir ]; then
+    echo "initialize $inst_dir"
+    mkdir $inst_dir
+    cp -a $piccolo/{src_Core,src_Testbench} $inst_dir/
+    mkdir -p $inst_dir/build
+fi
+
+cd $inst_dir/build
+# stdin of this script is passed through to build.sh
+exec "$work/build.sh" "$piccolo"
