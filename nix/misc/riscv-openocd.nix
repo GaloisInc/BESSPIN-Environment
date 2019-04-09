dca22eae3770febfdba20eb7e211f39078d80b7e
Stuart Pernsteiner <spernsteiner@galois.com>
2019-04-09 15:51:48 -0700

    switch openocd revision

--- nix/misc/riscv-openocd.nix
+++ nix/misc/riscv-openocd.nix
@@ -8,8 +8,8 @@ stdenv.mkDerivation rec {
   src     = fetchFromGitHub {
     owner  = "riscv";
     repo   = "riscv-openocd";
-    rev    = "35eed36ffdd082f5abfc16d4cc93511f6e225284";
-    sha256 = "00b9jy3kq8y04yprzvf7f1cnvg4nqmplnipaqk7iawvan1mnddw8";
+    rev    = "eb7af6cba0b42ea6d0990f4360a32ca90b7902fb";
+    sha256 = "1zc8l55pkndm4q8cmhq2haa01f1plhqc3smx5pa7p7gglzhnyl0v";
     fetchSubmodules = true;
   };
 
