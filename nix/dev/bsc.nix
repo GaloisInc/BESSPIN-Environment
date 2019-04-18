ecb75838e82cb8765c3c3df35d291cfc80393938
Stuart Pernsteiner <spernsteiner@galois.com>
2019-04-18 09:53:27 -0700

    add dev shell for building bsc

---
+++ nix/dev/bsc.nix
@@ -0,0 +1,8 @@
+let
+  pkgs = import ../pinned-pkgs.nix {};
+  inherit (pkgs) mkShell callPackage;
+  besspin = callPackage ../besspin-pkgs.nix { binaryLevel = 0; };
+
+in mkShell {
+  inputsFrom = with besspin; [ aeExportBsv ];
+}
