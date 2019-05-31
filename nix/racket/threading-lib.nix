a9a4ee78d5660b42d2b6158dafdb9d124547258c
Stuart Pernsteiner <spernsteiner@galois.com>
2019-05-31 16:20:56 -0700

    add missing threading-lib.nix

---
+++ nix/racket/threading-lib.nix
@@ -0,0 +1,13 @@
+{ mkRacketDerivation }:
+
+mkRacketDerivation rec {
+  pname = "threading-lib";
+  version = "1.2";
+  src = builtins.fetchGit {
+    url = "https://github.com/lexi-lambda/threading";
+    rev = "5490317b2ec03316cd87f84625bb8dde4c391a2a";
+  };
+  subdir = "threading-lib";
+
+  racketDeps = [];
+}
