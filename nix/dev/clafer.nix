89e71deef04bdc7fa84e2652ed36ae6d4f1b1e40
Stuart Pernsteiner <spernsteiner@galois.com>
2019-06-07 11:05:36 -0700

    add clafer dev shell

---
+++ nix/dev/clafer.nix
@@ -0,0 +1,22 @@
+{}:
+
+let
+  pkgs = import ../pinned-pkgs.nix {};
+  inherit (pkgs) mkShell callPackage lib;
+  besspin = callPackage ../besspin-pkgs.nix { binaryLevel = 0; };
+
+in mkShell {
+  inputsFrom = with besspin; [
+    haskellEnv.clafer_0_5_0.env
+  ];
+  buildInputs = with pkgs; with besspin; [
+    alloy alloy-check
+
+    # Locale DB is required to use UTF-8 locales inside the nix-shell.
+    glibcLocales
+  ];
+
+  LANG = "en_US.UTF-8";
+
+  nixpkgs = pkgs.path;
+}
