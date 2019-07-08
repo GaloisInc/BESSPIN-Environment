aa92f7b7175828edd8fc4907f19d357d1d27c9d8
Stuart Pernsteiner <spernsteiner@galois.com>
2019-07-08 16:25:55 -0700

    add packages for rocket-chip-config-plugin and for its output

--- nix/besspin/arch-extract-src.nix
+++ nix/besspin/arch-extract-src.nix
@@ -2,5 +2,5 @@
 
 builtins.fetchGit {
   url = "git@gitlab-ext.galois.com:ssith/arch-extract.git";
-  rev = "14633fec51f8d4d975c3f5d3ac0a6638afe5b105";
+  rev = "07a4f256e3783d766b582f88226538c646fed4f3";
 }
