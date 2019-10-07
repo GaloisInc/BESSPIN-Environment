323252c725571eb19c72ff49fe9ff204564084d9
Stuart Pernsteiner <spernsteiner@galois.com>
2019-10-07 10:02:45 -0700

    move some bootmem build scripts into the gfe repo

--- nix/gfe/chainloader-initramfs.nix
+++ nix/gfe/chainloader-initramfs.nix
@@ -9,21 +9,12 @@ stdenv.mkDerivation rec {
   configurePhase = "";
 
   buildPhase = ''
-    # Busybox components, all squashed to owner 0:0
-    gen_initramfs_list.sh -u squash -g squash ${riscvBusybox} >bootmem/busybox.files
-
-    gen_initramfs_list.sh -d >bootmem/default.files
-
-    cat >bootmem/chainloader.files <<EOF
-    file /init ${./chainloader-init} 0755 0 0
-    EOF
-
-    cat bootmem/busybox.files bootmem/default.files bootmem/chainloader.files | \
-      gen_init_cpio -t $SOURCE_DATE_EPOCH - | \
-      gzip --best >bootmem/chainloader.cpio.gz
+    bootmem/build_chainloader_initramfs.sh
   '';
 
   installPhase = ''
-    cp bootmem/chainloader.cpio.gz $out
+    cp bootmem/chainloader-initramfs.cpio.gz $out
   '';
+
+  BUSYBOX_PREFIX = riscvBusybox;
 }
