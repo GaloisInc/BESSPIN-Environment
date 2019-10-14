0b706126827c61cabd9535cf0d6aa325944b1457
Stuart Pernsteiner <spernsteiner@galois.com>
2019-10-14 11:42:56 -0700

    add testgen unpacker

--- nix/besspin/testgen-src.nix
+++ nix/besspin/testgen-src.nix
@@ -1,6 +1,17 @@
-{}:
+{ assembleSubmodules }:
 
-builtins.fetchGit {
-  url = "git@gitlab-ext.galois.com:ssith/testgen.git";
-  rev = "1071483a64e4a25b17e500fc3cff0de1b8df3466";
+let
+  fetchSsith = name: rev: args: builtins.fetchGit ({
+    name = "${name}-source";
+    url = "git@gitlab-ext.galois.com:ssith/${name}.git";
+    inherit rev;
+  } // args);
+
+in assembleSubmodules {
+  name = "testgen-source";
+  modules = {
+    "." = fetchSsith "testgen" "7a58ae226c2299810bf411d03e72fee885c5ef23" {};
+    "poc-exploits" = fetchSsith "poc-exploits"
+      "6a7a98cb0aa8fbcbc53084b656bfa8edb2718b96" { ref = "develop"; };
+  };
 }
