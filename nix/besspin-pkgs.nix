039dec9e18636ab1d33b3e918a906a800a8424f0
Stuart Pernsteiner <spernsteiner@galois.com>
2019-10-22 16:29:38 -0700

    revert changes introduced by merge

--- nix/besspin-pkgs.nix
+++ nix/besspin-pkgs.nix
@@ -93,9 +93,6 @@ let
       cbor2
       # Used by Nix binary cache deployment scripts
       requests
-    ]);
-
-    python2 = pkgs.python27.withPackages (ps: with ps; [
       # Dependencies of gfe's run_elf.py
       pyserial pexpect configparser
     ]);
@@ -167,10 +164,8 @@ let
     csmith-bof = callPackage cxx/csmith.nix {};
 
     # These riscv-arch values are taken from the coremark -march flags for P1/P2
-    riscv-gcc = callPackage misc/riscv-gcc.nix { riscv-arch = "rv32imac"; };
-    riscv-gcc-64 = callPackage misc/riscv-gcc.nix { riscv-arch = "rv64imafdc"; };
-    riscv-gcc-64-linux = callPackage misc/riscv-gcc.nix {
-      riscv-arch = "rv64imafdc";
+    riscv-gcc = callPackage misc/riscv-gcc.nix {};
+    riscv-gcc-linux = callPackage misc/riscv-gcc.nix {
       targetLinux = true;
     };
 
