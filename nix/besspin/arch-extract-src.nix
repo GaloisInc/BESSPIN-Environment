fa287946b3bb91639bdab9c89feb790e7b72ef6b
Stuart Pernsteiner <spernsteiner@galois.com>
2019-12-03 14:27:13 -0800

    bump arch-extract revision to pick up a bug fix

--- nix/besspin/arch-extract-src.nix
+++ nix/besspin/arch-extract-src.nix
@@ -2,5 +2,5 @@
 
 fetchGit2 {
   url = "git@gitlab-ext.galois.com:ssith/arch-extract.git";
-  rev = "8f0e6406859a15ae62fd8e88676f6703b8ad2a93";
+  rev = "9b7b3a9e4948edf766a538fe63439d3d9dfe4f50";
 }
