ca9d4cf95ae6d23a6980a7bedce6ade8cdc41c55
Ethan Lew <elew@galois.com>
2020-01-03 18:46:10 -0800

    add behav_srams.v, making P1/P2 complete

--- nix/scala/chisel-processors.nix
+++ nix/scala/chisel-processors.nix
@@ -1,4 +1,4 @@
-{stdenv, jre, rocket-chip, firrtl}:
+{python, stdenv, jre, rocket-chip, firrtl}:
 let 
     genDirName = "generated-src";
 
@@ -6,6 +6,7 @@ let
     galoisSystemSrc = procName:
         rocket-chip.overrideAttrs(old: {
             name = "galois-system-${procName}-chisel-src"; 
+
             preBuild = '' mkdir -p /tmp/${genDirName} '';
             buildPhase =''
                 mkdir -p /tmp/${genDirName}
@@ -16,7 +17,9 @@ let
             '';
             installPhase = ''
                 mkdir -p $out/${genDirName}
+                mkdir -p $out/scripts
                 cp -R /tmp/${genDirName} $out
+                cp -R scripts $out
             '';
         });
 
@@ -30,12 +33,30 @@ let
             buildInputs = [ jre ];
             installPhase = ''
                 mkdir -p $out/${targetDir}
-                ${firrtl}/bin/firrtl -i ${firrtlSrc}/${genDirName}/galois.system.${procName}TVFPGAConfig.fir -o galois.system.${procName}TVFPGAConfig.v -X verilog --infer-rw TestHarness  --repl-seq-mem -c:TestHarness:-o:$out/${targetDir}/galois.system.${procName}TVFPGAConfig.conf -faf ${firrtlSrc}/${genDirName}/galois.system.${procName}TVFPGAConfig.anno.json -td $out/${targetDir}
+                ${firrtl}/bin/firrtl -i ${firrtlSrc}/${genDirName}/galois.system.${procName}TVFPGAConfig.fir -o $out/${targetDir}/galois.system.${procName}TVFPGAConfig.v -X verilog --infer-rw TestHarness --repl-seq-mem -c:TestHarness:-o:$out/${targetDir}/galois.system.${procName}TVFPGAConfig.conf  -td $out/${targetDir}/galois.system.${procName}TVFPGAConfig/ 
             '';
-        };
-in {
+          };
+
+    makeProcessor = procName:
+          let
+            psrc = galoisSystemSrc procName;
+            pfirrtl = firrtlTransformSrc psrc procName;
+            targetDir = "galois.system.${procName}TVFPGAConfig";
+          in stdenv.mkDerivation {
+            name = "chisel-processor-${procName}";
+            src = psrc;
+            patches = [ ./vlsi_mem.patch ];
+            buildInputs = [ python pfirrtl ];
+            buildPhase = ''
+            python ${psrc}/scripts/vlsi_mem_gen ${pfirrtl}/${targetDir}/galois.system.${procName}TVFPGAConfig.conf > /tmp/galois.system.${procName}TVFPGAConfig.behav_srams.v 
+            '';
+            installPhase = ''
+              mkdir $out
+              cp -R /tmp/* $out
+            '';
+          };
+
     p1-src = galoisSystemSrc "P1";
     p2-src = galoisSystemSrc "P2";
-    p1-firrtl = firrtlTransformSrc p1-src "P1";
-    p2-firrtl = firrtlTransformSrc p1-src "P2";
-}
+
+in makeProcessor "P2"
