bddeebe3c553103d4b310619ab63507de393b39a
Alexander Bakst <abakst@galois.com>
2020-01-13 09:03:41 -0800

    Update pointer to arch-extract (new tmtool features).

--- nix/besspin/arch-extract-src.nix
+++ nix/besspin/arch-extract-src.nix
@@ -2,5 +2,5 @@
 
 fetchGit2 {
   url = "git@gitlab-ext.galois.com:ssith/arch-extract.git";
-  rev = "94c8810f5465b89f4f2195e58e3bac55fb709190";
+  rev = "15d7ba92e3025195cb8af71353e8e683aa1cb7fc";
 }
