ff55d95fa4fbe0b8ea7fd42c78c77767f8dbf374
Isaiah Mindich <isaiah@galois.com>
2020-01-16 11:29:02 -0800

    generalize bluespec verilog package

--- nix/gfe/bluespec-verilog.nix
+++ nix/gfe/bluespec-verilog.nix
@@ -1,16 +1,17 @@
 { stdenv
 , bscBinary
-, gfeSrc
+, src
+, gfe-target
 }:
 
 stdenv.mkDerivation rec {
-  name = "besspin-p1-verilog";
+  name = "bluespec-${gfe-target}-verilog";
 
   # We can't use sandboxing since bsc needs access to the eth0 network
   # interface in order to check the license.
   __noChroot = true;
 
-  src = gfeSrc.modules."bluespec-processors/P1/Piccolo";
+  inherit src;
 
   buildInputs = [ bscBinary ];
 
@@ -19,14 +20,11 @@ stdenv.mkDerivation rec {
   # any publicly available version of bsc, but everything compiles
   # just fine if we remove it.
   buildPhase = ''
-    cd src_SSITH_P1
-    sed s/-no-show-timestamps// Makefile >Makefile_new
+    cd src_SSITH_${gfe-target}
+    sed s/-no-show-timestamps// Makefile >Makefile.new
     export BLUESPEC_LICENSE_FILE=/opt/Bluespec-2017.07.A/bluespec.lic
-    make -f Makefile_new compile
+    make -f Makefile.new compile
   '';
-
-  outputHashMode = "recursive";
-  outputHashAlgo = "sha256";
 
   installPhase = ''
     mkdir -p $out
