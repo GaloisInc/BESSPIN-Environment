2d9e350514188274105def3020e952634ea7fcff
Max Orhai <max.orhai@galois.com>
2020-01-30 11:34:28 -0800

    Package pyprogram_fpga.py.

--- nix/gfe/program-fpga.nix
+++ nix/gfe/program-fpga.nix
@@ -28,7 +28,7 @@ in stdenv.mkDerivation rec {
   installPhase = ''
     mkdir -p $out/bitstreams
 
-    cp program_fpga.sh setup_env.sh $out
+    cp pyprogram_fpga.py program_fpga.sh setup_env.sh $out
 
     for d in ${lib.concatStringsSep " " bitstreamDirs}; do
       cp $d/* $out/bitstreams
