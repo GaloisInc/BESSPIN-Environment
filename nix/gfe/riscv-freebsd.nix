0206ca0c1b69c007d93d6581413bacb3d087e108
Ethan Lew <elew@galois.com>
2020-02-05 12:32:07 -0800

    change riscv-freebsd.nix to fetch from artifactory binary

--- nix/gfe/riscv-freebsd.nix
+++ nix/gfe/riscv-freebsd.nix
@@ -1,11 +1,7 @@
 { fetchurl }:
 { 
- image =  fetchurl {
-    url = "file:///temp-build/riscv.img";
-    sha256 = "04hl8z9f07vmnwrlgd1n6g27v1lx443lwawdx04rw7xlcmlljrfy";
-  };
-  bbl = fetchurl {
-    url = "file:///temp-build/bbl";
-    sha256 = "1whc0r1iysgi718n9h4prn5vcwmfl07j7q8aldvsbznk7w84dfvk";
+ image =  builtins.fetchurl{
+    url = "https://artifactory.galois.com:443/besspin_generic-nix/FreeBSDQemu.elf";
+    sha256 = "57a89a4f92a18013a3cff6185f368dadf54e99fe1adf3d0a44671f1e16ddca88";
   };
 }
