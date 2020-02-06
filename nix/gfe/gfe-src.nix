6bac62a3f50f7d5cddf015ea27707007b32eea78
Andrew Bivin <abivin@galois.com>
2020-02-06 11:52:41 -0800

    Update gfe-src.nix sha256

--- nix/gfe/gfe-src.nix
+++ nix/gfe/gfe-src.nix
@@ -35,7 +35,7 @@ in assembleSubmodules {
   # on large sources that aren't used for any packages at the moment.
   modules = {
     "." = togglePackagePerf "gfe"
-      "0a546d081krwsi5nvxgkxxxxxxwinm312nmgnx2b3rk43c3wpmlx"
+      "0wahjj0kffkxscic51xjjfhz6ias4zlzhysim72lkkpjchzyzg4q"
       (fetchSsith "gfe" "pytest-processor-chdir"
         "01878fc96836de571f78a7d7a9be806cb0d208c8" {})
       "01878fc96836de571f78a7d7a9be806cb0d208c8";
