63b07e990eb5fc6a26f328edb4273e4608773dfc
Ethan Lew <elew@galois.com>
2020-02-07 15:24:51 -0800

    switch to nixpkgs fetchurl for riscv-freebsd.nix package for binary cache usage

--- nix/gfe/riscv-freebsd.nix
+++ nix/gfe/riscv-freebsd.nix
@@ -1,7 +1,7 @@
 { fetchurl }:
 { 
- image =  builtins.fetchurl{
-    url = "https://artifactory.galois.com:443/besspin_generic-nix/FreeBSDQemu.elf";
-    sha256 = "57a89a4f92a18013a3cff6185f368dadf54e99fe1adf3d0a44671f1e16ddca88";
+ image =  fetchurl {
+	url = "http://localhost:8000/FreeBSDQemu.elf";
+    	sha256 = "57a89a4f92a18013a3cff6185f368dadf54e99fe1adf3d0a44671f1e16ddca88";
   };
 }
