61cfc92dfeb73e028d31995912288329ccd1c879
Ethan Lew <elew@galois.com>
2020-02-07 16:25:04 -0800

    Revert "Merge branch 'cheri-freebsd' into 'develop'"
    This reverts merge request !48

--- nix/besspin-pkgs.nix
+++ nix/besspin-pkgs.nix
@@ -181,8 +181,6 @@ let
     # Csmith, built from the galois `bof` branch.
     csmith-bof = callPackage cxx/csmith.nix {};
 
-    cheri-qemu = callPackage ./misc/cheri/cheri-qemu.nix {};
-
     # These riscv-arch values are taken from the coremark -march flags for P1/P2
     riscv-gcc = callPackage misc/riscv-gcc.nix {};
     riscv-gcc-linux = callPackage misc/riscv-gcc.nix {
