77bcf7aea9368c9c33955263be65ab4de641a634
Isaiah Mindich <isaiah@galois.com>
2020-02-11 11:39:15 -0800

    add dev shell for freebsd

---
+++ nix/dev/freebsd.nix
@@ -0,0 +1,10 @@
+let
+  pkgs = import ../pinned-pkgs.nix {};
+  inherit (pkgs) mkShell callPackage;
+  besspin = callPackage ../besspin-pkgs.nix {};
+
+in mkShell {
+  buildInputs = with pkgs; with besspin; [
+    freebsd
+  ];
+}
