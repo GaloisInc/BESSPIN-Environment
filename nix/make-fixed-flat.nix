c5cfba2e89adc7231bd038a9476162f6f1c6ada6
Andrew Bivin <abivin@galois.com>
2020-02-14 08:32:08 -0800

    Revert "Merge branch 'test-freebsd-edits' into 'develop'"
    This reverts merge request !53

--- nix/make-fixed-flat.nix
+++
@@ -1,9 +0,0 @@
-# A version of makeFixed for single file store paths
-{}:
-name: sha256: pkg:
-pkg.overrideAttrs (old: {
-  name = name + "-fixed";
-  outputHash = sha256;
-  outputHashAlgo = "sha256";
-  outputHashMode = "flat";
-})
