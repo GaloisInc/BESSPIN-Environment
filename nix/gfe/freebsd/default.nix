2098bbe47cac503621b88498cec3b6e7c835558e
Ethan Lew <elew@galois.com>
2020-02-14 17:47:57 -0800

    push build variables in a derivation creation function, add kernel derivation function

--- nix/gfe/freebsd/default.nix
+++ nix/gfe/freebsd/default.nix
@@ -1,24 +1,110 @@
-pkgs@{stdenv
+pkgs @ { stdenv
 , bmake
 , lib
 , newScope
+, fetchFromGitHub
+, riscv-clang
+, riscv-llvm
+, riscv-lld
+, clangStdenv
+, which
+, python3
+, libarchive
+, hostname
+, zlib
+, bash
 , overrides ? (self: super: {})
 }:
 
 
 let
+  freebsdSrc = fetchFromGitHub {
+    owner = "arichardson";
+    repo = "freebsd";
+    rev = "75dd3963e6b8eb7c9fca9e6fb55f51feb1bd17d5";
+    sha256 = "1a509nyhwyw5wwpsjdpbrx2nyipxibfx28nb368nyqhdzq1xanwk";
+  };
+  
+  bmakeFlagsMinimal = [
+    "-DDB_FROM_SRC"
+    "-DNO_ROOT"
+    "-DBUILD_WITH_STRICT_TMPPATH"
+    "TARGET=riscv"
+    "TARGET_ARCH=riscv64"
+    "-DNO_WERROR WERROR="
+    "-DWITHOUT_TESTS"
+    "-DWITHOUT_MAN"
+    "-DWITHOUT_MAIL"
+    "-DWITHOUT_VT"
+    "-DWITHOUT_DEBUG_FILES"
+    "-DWITHOUT_BOOT"
+    "-DWITH_AUTO_OBJ"
+    "-DWITHOUT_GCC_BOOTSTRAP"
+    "-DWITHOUT_CLANG_BOOTSTRAP"
+    "-DWITHOUT_LLD_BOOTSTRAP"
+    "-DWITHOUT_LIB32"
+    "-DWITH_ELFTOOLCHAIN_BOOTSTRAP"
+    "-DWITHOUT_TOOLCHAIN"
+    "-DWITHOUT_BINUTILS_BOOTSTRAP"
+    "-DWITHOUT_RESCUE"
+    "-DWITHOUT_BLUETOOTH"
+    "-DWITHOUT_SVNLITE"
+    "-DWITHOUT_CDDL"
+    "-DWITHOUT_PF"
+    "-DWITHOUT_PROFILE"
+    "-DWITHOUT_VI"
+    "-DWITHOUT_SYSCONS"
+    "-DWITHOUT_CTF"
+    "-DWITHOUT_LOCALES"
+    "-DWITHOUT_DICT"
+    "-DWITHOUT_EXAMPLES"
+    "-DWITHOUT_HTML"
+    "-DWITHOUT_FILE"
+    "-DWITHOUT_MAKE"
+    "-DWITHOUT_PORTSNAP"
+    "-DWITHOUT_PKGBOOTSTRAP"
+    "-DWITHOUT_OPENMP"
+    "-DWITHOUT_SHAREDOCS"
+    "-DWITHOUT_WIRELESS"
+    "-DWITHOUT_KDUMP"
+    "-DWITHOUT_AUDIT"
+    "-DWITHOUT_TFTP"
+    "-DWITHOUT_CXGBETOOL"
+    "-DWITHOUT_LDNS"
+    "-DWITHOUT_QUOTAS"
+    "-DWITHOUT_TALK"
+    "-DWITHOUT_USB"
+    "-DWITHOUT_NLS"
+    "-DWITHOUT_UTMPX"
+    "-DWITHOUT_KERNEL_SYMBOLS"
+    "-DWITHOUT_OPENSSH"
+    "-DWITHOUT_KERBEROS"
+    "MODULES_OVERRIDE="
+  ];
+
+  version = "12.1";
+  
   targets = self: rec {
+    bmakeFlags = bmakeFlagsMinimal;
+    inherit version;
+
+    src = freebsdSrc;
+
     callPackage = pkgs.newScope self;
     newScope = extra: pkgs.newScope (self // extra);
-
-    freebsdWorld = callPackage ./freebsd.nix {
-      inherit bmake;
+    mkFreebsdDerivation = callPackage ./freebsd.nix { inherit bmake; };
+    
+    freebsdWorld = callPackage ./freebsd-world.nix {inherit mkFreebsdDerivation;
+      src = freebsdSrc;
     };
+
     freebsdImage = callPackage ./freebsd-rootfs-image.nix { };
     freebsdKernelQemu = callPackage ./freebsd-kernel.nix {
+      inherit mkFreebsdDerivation;
       device = "QEMU";
     };
     freebsdKernelFpga = callPackage ./freebsd-kernel.nix {
+      inherit mkFreebsdDerivation;
       device = "FPGA";
     };
   };
--- nix/gfe/freebsd/freebsd.nix
+++ nix/gfe/freebsd/freebsd.nix
@@ -2,7 +2,6 @@
 , riscv-clang
 , riscv-llvm
 , riscv-lld
-, fetchFromGitHub
 , clangStdenv
 , bmake
 , which
@@ -11,142 +10,70 @@
 , libarchive
 , hostname
 , zlib
-, bash
-, enableTools ? true
-, enableSource ? true
-}:
+, bash}:
 
-clangStdenv.mkDerivation rec {
-  pname = "freebsd";
-  version = "12.1";
+let 
+  mkFreebsdDerivation = { bmakeFlags ? []
+    , bmakeTargets ? [ "buildworld" "buildkernel" ]
+    , tname
+    , version
+    , src 
+    }:
+    clangStdenv.mkDerivation rec {
+      pname = "freebsd-${tname}";
 
-  src = fetchFromGitHub {
-    owner = "arichardson";
-    repo = "freebsd";
-    rev = "75dd3963e6b8eb7c9fca9e6fb55f51feb1bd17d5";
-    sha256 = "1a509nyhwyw5wwpsjdpbrx2nyipxibfx28nb368nyqhdzq1xanwk";
-  };
+      buildInputs = [
+        bmake
+        libarchive
+        which
+        python3
+        hostname
+        zlib
+      ];
 
-  buildInputs = [
-    bmake
-    libarchive
-    which
-    python3
-    hostname
-    zlib
-  ];
+      inherit src version bmakeTargets bmakeFlags;
 
-  phases = [ "unpackPhase" "patchPhase" "buildPhase" "installPhase" ];
+      XCC = "${riscv-clang}/bin/clang";
+      XCXX = "${riscv-clang}/bin/clang++";
+      XCPP = "${riscv-clang}/bin/clang-cpp";
+      XLD = "${riscv-lld}/bin/ld.lld";
+      XOBJDUMP = "${riscv-llvm}/bin/llvm-objdump";
+      XOBJCOPY = "${riscv-llvm}/bin/llvm-objcopy";
+      XCFLAGS = "-fuse-ld=${riscv-lld}/bin/ld.lld -Qunused-arguments";
 
-  XCC = "${riscv-clang}/bin/clang";
-  XCXX = "${riscv-clang}/bin/clang++";
-  XCPP = "${riscv-clang}/bin/clang-cpp";
-  XLD = "${riscv-lld}/bin/ld.lld";
-  XOBJDUMP = "${riscv-llvm}/bin/llvm-objdump";
-  XOBJCOPY = "${riscv-llvm}/bin/llvm-objcopy";
-  XCFLAGS = "-fuse-ld=${riscv-lld}/bin/ld.lld -Qunused-arguments";
+      patchPhase = ''
+        # Replace absolute paths in makefiles.
+        sed 's@/bin/bash@${bash}/bin/bash@' -i tools/build/Makefile
+        sed 's@/usr/bin/ar@ar@' -i tools/build/mk/Makefile.boot
+        sed 's@/usr/bin/nm@nm@' -i tools/build/mk/Makefile.boot
+        sed 's@/usr/bin/ranlib@ranlib@' -i tools/build/mk/Makefile.boot
+        sed 's@/usr/bin/env@env@' -i Makefile
+        mkdir locale
+        sed -i "s@/usr/share/locale@$(realpath locale)@" Makefile.inc1
 
-  bmakeFlags = [
-    "-DDB_FROM_SRC"
-    "-DNO_ROOT"
-    "-DBUILD_WITH_STRICT_TMPPATH"
-    "TARGET=riscv"
-    "TARGET_ARCH=riscv64"
-    "-DNO_WERROR WERROR="
-    "-DWITHOUT_TESTS"
-    "-DWITHOUT_MAN"
-    "-DWITHOUT_MAIL"
-    "-DWITHOUT_VT"
-    "-DWITHOUT_DEBUG_FILES"
-    "-DWITHOUT_BOOT"
-    "-DWITH_AUTO_OBJ"
-    "-DWITHOUT_GCC_BOOTSTRAP"
-    "-DWITHOUT_CLANG_BOOTSTRAP"
-    "-DWITHOUT_LLD_BOOTSTRAP"
-    "-DWITHOUT_LIB32"
-    "-DWITH_ELFTOOLCHAIN_BOOTSTRAP"
-    "-DWITHOUT_TOOLCHAIN"
-    "-DWITHOUT_BINUTILS_BOOTSTRAP"
-    "-DWITHOUT_RESCUE"
-    "-DWITHOUT_BLUETOOTH"
-    "-DWITHOUT_SVNLITE"
-    "-DWITHOUT_CDDL"
-    "-DWITHOUT_PF"
-    "-DWITHOUT_PROFILE"
-    "-DWITHOUT_VI"
-    "-DWITHOUT_SYSCONS"
-    "-DWITHOUT_CTF"
-    "-DWITHOUT_LOCALES"
-    "-DWITHOUT_DICT"
-    "-DWITHOUT_EXAMPLES"
-    "-DWITHOUT_HTML"
-    "-DWITHOUT_FILE"
-    "-DWITHOUT_MAKE"
-    "-DWITHOUT_PORTSNAP"
-    "-DWITHOUT_PKGBOOTSTRAP"
-    "-DWITHOUT_OPENMP"
-    "-DWITHOUT_SHAREDOCS"
-    "-DWITHOUT_WIRELESS"
-    "-DWITHOUT_KDUMP"
-    "-DWITHOUT_AUDIT"
-    "-DWITHOUT_TFTP"
-    "-DWITHOUT_CXGBETOOL"
-    "-DWITHOUT_LDNS"
-    "-DWITHOUT_QUOTAS"
-    "-DWITHOUT_TALK"
-    "-DWITHOUT_USB"
-    "-DWITHOUT_NLS"
-    "-DWITHOUT_UTMPX"
-    "-DWITHOUT_KERNEL_SYMBOLS"
-    "-DWITHOUT_OPENSSH"
-    "-DWITHOUT_KERBEROS"
-    "MODULES_OVERRIDE="
-  ];
+        # Change the default PATH defined in the main makefile. Trying to
+        # do this by passing in a make flag breaks everything for some reason.
+        sed -i "s!^PATH=.*!PATH=\t$PATH!" Makefile
 
-  patchPhase = ''
-    # Replace absolute paths in makefiles.
-    sed 's@/bin/bash@${bash}/bin/bash@' -i tools/build/Makefile
-    sed 's@/usr/bin/ar@ar@' -i tools/build/mk/Makefile.boot
-    sed 's@/usr/bin/nm@nm@' -i tools/build/mk/Makefile.boot
-    sed 's@/usr/bin/ranlib@ranlib@' -i tools/build/mk/Makefile.boot
-    sed 's@/usr/bin/env@env@' -i Makefile
-    mkdir locale
-    sed -i "s@/usr/share/locale@$(realpath locale)@" Makefile.inc1
+        # GNU and BSD date have different options.
+        sed -i 's/date -r $SOURCE_DATE_EPOCH/date -d @$SOURCE_DATE_EPOCH/' \
+          sys/conf/newvers.sh
+      '';
 
-    # Change the default PATH defined in the main makefile. Trying to
-    # do this by passing in a make flag breaks everything for some reason.
-    sed -i "s!^PATH=.*!PATH=\t$PATH!" Makefile
+      setOutputFlags = false;
 
-    # GNU and BSD date have different options.
-    sed -i 's/date -r $SOURCE_DATE_EPOCH/date -d @$SOURCE_DATE_EPOCH/' \
-      sys/conf/newvers.sh
-  '';
-
-  outputs = [ "out" ] ++ lib.optional enableSource "source" ++ lib.optional enableTools "tools" ;
-  setOutputFlags = false;
-
-  bmakeTargets = [ "buildworld" ];
-  
-  buildPhase = ''
-    unset STRIP
-    mkdir -p obj
-    export MAKEOBJDIRPREFIX=$PWD/obj
-      ${lib.concatMapStringsSep "\n" (tgt: ''bmake -de $bmakeFlags \
-      'LOCAL_XTOOL_DIRS=lib/libnetbsd usr.sbin/makefs usr.bin/mkimg' \
-      ${tgt} -j$NIX_BUILD_CORES 
-    '') bmakeTargets}
-  '';
-  
-  installPhase = ''
-    mkdir -p $out/world
-    bmake -de DESTDIR=$out/world $bmakeFlags installworld
-    bmake -de DESTDIR=$out/world $bmakeFlags distribution
-  '' + lib.optionalString enableSource ''
-    mkdir $source
-    cp -R * $source
-  '' + lib.optionalString enableTools ''
-    TMPDIR=obj/$(realpath .)/riscv.riscv64/tmp
-    mkdir -p $tools/bin
-    cp $TMPDIR/usr/sbin/makefs $TMPDIR/usr/bin/mkimg $tools/bin
-  '';
-}
+      buildPhase = ''
+        unset STRIP
+        mkdir -p obj
+          ${lib.concatMapStringsSep "\n" (tgt: ''echo "bmake -de $bmakeFlags \
+          'LOCAL_XTOOL_DIRS=lib/libnetbsd usr.sbin/makefs usr.bin/mkimg' \
+          ${tgt} -j$NIX_BUILD_CORES" 
+        '') bmakeTargets}
+        export MAKEOBJDIRPREFIX=$PWD/obj
+          ${lib.concatMapStringsSep "\n" (tgt: ''bmake -de $bmakeFlags \
+          'LOCAL_XTOOL_DIRS=lib/libnetbsd usr.sbin/makefs usr.bin/mkimg' \
+          ${tgt} -j$NIX_BUILD_CORES 
+        '') bmakeTargets}
+      '';
+    };
+in mkFreebsdDerivation
