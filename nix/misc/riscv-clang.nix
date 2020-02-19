a6daf724aa781cdea4a8d682b5eaa1649999509c
Isaiah Mindich <isaiah@galois.com>
2020-02-18 16:13:44 -0800

    Update to clang 11

--- nix/misc/riscv-clang.nix
+++ nix/misc/riscv-clang.nix
@@ -1,37 +1,34 @@
-{ stdenv, lib, fetchFromGitHub2, llvmPackages_9, fetchurl }:
+{ stdenv, lib, llvmPackages_9, fetchurl, fetchFromGitHub }:
 
 let
-  releaseVersion = "10.0.0-rc1";
-  llvmVersion = "10.0.0rc1";
+  version = "11.0.0-git";
 
-  fetchRelease = name: sha256: fetchurl {
-    url = "https://github.com/llvm/llvm-project/releases/download/llvmorg-${releaseVersion}/${name}-${llvmVersion}.src.tar.xz";
-    inherit sha256;
+  fetchSrc = projName: archiveName: stdenv.mkDerivation rec {
+    name = archiveName + "-${version}.tar.gz";
+    src = fetchFromGitHub {
+      owner = "llvm";
+      repo = "llvm-project";
+      rev = "2d146aa2a2cdef330877b511b54886823e71f92c";
+      sha256 = "10q7qd15b8fpcsp91hzx83i52v8ngwws54nkk9ymz792d0ymrfjk";
+    };
+    phases = [ "unpackPhase" "installPhase" ];
+    installPhase = ''
+      mv ${projName} ${archiveName}-${version} && tar -czf $out ${archiveName}-${version}
+    '';
   };
 
   fetch = name: sha256:
-    if name == "llvm" then
-      fetchRelease "llvm"
-        "0c77jbr7r2y57gqc8cxx8bz5p1zjl6pvsm1gajlvr77j8a5g1pia"
-    else if name == "cfe" then
-      fetchRelease "clang"
-        "1njqwnakj73ga3fbqh6y2cmxswx998prb1m6drda3j26ss9wrw6p"
-    else if name == "clang-tools-extra" then
-      fetchRelease "clang-tools-extra"
-        "0fpmsdsv3nc270pbv5qry6gk0sp9vnpcgw98y256n6q52m4lbcfj"
-    else if name == "lld" then
-      fetchRelease "lld"
-        "09mfa3vl7saycj1pha9bsa1l6544yi74cqhcn2a7p5lw4k36laww"
-    else if name == "polly" then
-      fetchRelease "polly"
-        "1i8nfx81i7rs26yw9y2yxn3fbfl48vivqg2pbwyrh8bl7v685l4p"
+    if name == "llvm" then fetchSrc "llvm" "llvm"
+    else if name == "cfe" then fetchSrc "clang" "cfe"
+    else if name == "clang-tools-extra" then fetchSrc "clang-tools-extra" "clang-tools-extra"
+    else if name == "lld" then fetchSrc "lld" "lld"
+    else if name == "polly" then fetchSrc "polly" "polly"
     else
       abort "unexpected repo in fetch: ${name}";
 
 in rec {
   llvm = (llvmPackages_9.llvm.override {
-    version = llvmVersion;
-    inherit fetch;
+    inherit version fetch;
   }).overrideAttrs (old: {
     # We only really care about cross compiling from amd64 to riscv,
     # so we can ignore most of the patches from the original package.
@@ -44,32 +41,26 @@ in rec {
   });
 
   clang = (llvmPackages_9.clang-unwrapped.override {
-    inherit llvm;
-    version = llvmVersion;
-    inherit fetch;
+    inherit llvm version fetch;
     clang-tools-extra_src = fetch "clang-tools-extra" "";
   }).overrideAttrs (old: {
-    #    patches = [ ./riscv-clang-purity.patch ]
-    unpackPhase = ''
-      unpackFile $src
-      mv clang-${llvmVersion}.src* clang
-      sourceRoot=$PWD/clang
-      unpackFile ${fetch "clang-tools-extra" ""}
-      mv clang-tools-extra-* $sourceRoot/tools/extra
-    '';
+    patches = [ ./riscv-clang-purity.patch ];
 
-    # LLVM 10 uses c++14
+    # LLVM 10 and later use c++14
     cmakeFlags = [
       "-DCMAKE_CXX_FLAGS=-std=c++14"
       "-DCLANGD_BUILD_XPC=OFF"
     ];
 
+    # The clang derivation in nixpkgs makes some changes to the driver
+    # in the postPatch phase which prevent clang from searching for
+    # headers using the standard system paths like
+    # /usr/include. Unfortunately these changes make clang ignore the
+    # --sysroot option, which we need for cross compilation
     postPatch = "";
   });
 
   lld = llvmPackages_9.lld.override {
-    version = llvmVersion;
-    inherit llvm;
-    inherit fetch;
+    inherit llvm version fetch;
   };
 }
