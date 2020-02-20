15dda0db8d496d40f19c33795033d91fa4c1b33b
Isaiah Mindich <isaiah@galois.com>
2020-02-20 14:11:47 -0800

    freebsd: don't install world in nested directory

--- nix/gfe/freebsd/freebsd-world.nix
+++ nix/gfe/freebsd/freebsd-world.nix
@@ -5,24 +5,24 @@
 , src
 , bmakeFlags
 }:
-  
+
 mkFreebsdDerivation {
   inherit version src bmakeFlags;
-  
+
   tname = "world";
-  
+
   bmakeTargets = [ "buildworld" ];
-  
+
   outputs = ["out" "source" "tools"];
-  
+
   installPhase = ''
-    mkdir -p $out/world
-    bmake -de DESTDIR=$out/world $bmakeFlags installworld
-    bmake -de DESTDIR=$out/world $bmakeFlags distribution
-    
+    mkdir -p $out
+    bmake -de DESTDIR=$out $bmakeFlags installworld
+    bmake -de DESTDIR=$out $bmakeFlags distribution
+
     mkdir $source
     cp -R * $source
-    
+
     TMPDIR=obj/$(realpath .)/riscv.riscv64/tmp
     mkdir -p $tools/bin
     cp $TMPDIR/usr/sbin/makefs $TMPDIR/usr/bin/mkimg $tools/bin
