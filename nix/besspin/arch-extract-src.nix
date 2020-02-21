09ccb82123b9be37e2881c94de9535522073f26b
Alexander Bakst <abakst@galois.com>
2020-02-21 14:27:08 -0800

    Update arch-extract-src pointer

--- nix/besspin/arch-extract-src.nix
+++ nix/besspin/arch-extract-src.nix
@@ -2,5 +2,5 @@
 
 fetchGit2 {
   url = "git@gitlab-ext.galois.com:ssith/arch-extract.git";
-  rev = "15d7ba92e3025195cb8af71353e8e683aa1cb7fc";
+  rev = "11fce9e6191ed50716651c4538726567f9e5646e";
 }
