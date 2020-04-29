5a6e47388348397dfee6ebec926b7d65e369e5a8
Ethan Lew <elew@galois.com>
2020-04-29 15:13:53 -0700

    review fixes, make default shell FETT

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,19 +1,20 @@
-pkgs@{ mkShell, callPackage, path
-, jre, go, graphviz, alloy, pandoc, openssl, bc, bison, flex, glibc, verilator
-, qemu, which, netcat, glibcLocales, xxd, ps
-}:
+pkgs@{ callPackage, mkShell, qemu, which, netcat, xxd, ps, tcl }:
 
 let
   besspin = callPackage ./besspin-pkgs.nix {};
 
-in mkShell {
-  buildInputs = with besspin; [
-    python3
-    rEnv
-    racket
-    sbt
-    go
+  fettPython3 = with besspin; python3Env.withPackages (ps: with ps; [
+    requests
+    pyserial
+    pexpect
+    configparser
+    scapy
+    tftpy
+    psutil
+  ]);
 
+in mkShell {
+  buildInputs = with besspin; with pkgs; [
     # RISCV toolchain
     riscv-gcc
     riscv-gcc-linux
@@ -23,137 +24,33 @@ in mkShell {
     riscv-llvm
     riscv-clang
     riscv-lld
-    # run_elf.py requires openocd in $PATH
-    riscv-openocd
 
-    verilator
-
-    graphviz
-    alloy
-    alloy-check
-    clafer
-    # We use the normal `pandoc` instead of `haskellEnv.pandoc` because the
-    # normal one will be available from the NixOS binary caches.
-    pandoc
-
-    # Used for riscv-linux build
-    openssl bc bison flex
-
-    # Deps of rocket-chip build process
-    jre
-    scalaEnv.scala
-    rocketChipBuildUnpacker
-
-    configuratorWrapper
-    halcyon
-    halcyonBoomUnpacker
-
-    testgenUnpacker
-
-    riscvTimingTests
-    rvttPlotInt
-    rvttInterpolate
-    rvttUnpacker
-
-    aeDriverWrapper
-    featuresynthWrapper
-    fmtoolWrapper
-    rocketChipHelper
-    #boomHelper   # temporarily unavailable - see tool-suite#63
-    rocketChipCheckConfigWrapper
-    # Needed for now, as there arch-extract can't export firrtl on its own yet
-    aeExportFirrtl
-
-    coremarkSrcUnpacker
-    coremarkBuildsUnpacker
-    buildPiccolo
-    mibenchSrcUnpacker
-    mibenchBuildsUnpacker
-    pocExploitsUnpacker
-
-    # User-facing GFE functions. See also dev/gfe.nix.
+    # User-facing GFE functions. See also gfe.nix
     programFpgaWrapper
     clearFlashWrapper
     runElf
 
-
     # testgen dependencies
-    python3
+    fettPython3
     qemu
-
-    riscv-gcc
-    riscv-gcc-linux
     riscv-libpam
     riscv-libkeyutils
-    riscv-llvm
-    riscv-clang
-    riscv-lld
     riscv-openocd
-
-    programFpgaWrapper
-    runElf
-    verilator
-    riscvTestsBuildUnpacker
-
-    simulatorBins
-    extractedArchitecturesUnpacker
-
     which
     netcat
     xxd
     ps
-
-    # Haskell programs fail to read UTF-8 inputs when locales are not
-    # installed, or when using a non-UTF-8 locale.
-    glibcLocales
-
+    
+    testgenUnpacker
+    tcl
   ];
 
-  nixpkgs = path;
-
-  # -Werror=format-security causes problems for some HOSTCC parts of the
-  # binutils build
-  hardeningDisable = [ "format" ];
-
-
-  # Used by the verilator simulator builds
-  GLIBC_STATIC = pkgs.glibc.static;
-
-  inherit (besspin) debianRepoSnapshot;
-
-  # More packages used by testgen
-  BESSPIN_TESTGEN_BUSYBOX_IMAGE_QEMU = besspin.busyboxImageQemu;
-  BESSPIN_TESTGEN_BUSYBOX_IMAGE = besspin.busyboxImage;
-  BESSPIN_TESTGEN_DEBIAN_IMAGE_QEMU = besspin.testgenDebianImageQemu;
-  BESSPIN_TESTGEN_DEBIAN_IMAGE = besspin.debianImage;
-  BESSPIN_TESTGEN_FREEBSD_IMAGE_QEMU = besspin.freebsdImageQemu;
-  BESSPIN_TESTGEN_FREEBSD_IMAGE = besspin.freebsdImage;
-  BESSPIN_TESTGEN_FREEBSD_DEBUG_IMAGE_QEMU = besspin.freebsdDebugImageQemu;
-  BESSPIN_TESTGEN_FREEBSD_DEBUG_IMAGE = besspin.freebsdDebugImage;
-  BESSPIN_TESTGEN_NETBOOT_IMAGE = besspin.netbootLoader;
-  BESSPIN_GFE_SCRIPT_DIR = "${besspin.testingScripts}/scripts";
-  BESSPIN_TESTGEN_PAM_DIR = besspin.riscv-libpam;
-  BESSPIN_TESTGEN_KEYUTILS_DIR = besspin.riscv-libkeyutils;
-
-  # Convenient list of packages referenced in the above environment variables,
-  # used to simplify deployment.
-  extraInputs = with besspin; [
-    pkgs.glibc.static
-    debianRepoSnapshot
-    busyboxImageQemu
-    busyboxImage
-    testgenDebianImageQemu
-    debianImage
-    testingScripts
-    riscv-libpam
-    riscv-libkeyutils
-    freebsdImageQemu
-    freebsdImage
-    freebsdDebugImageQemu
-    freebsdDebugImage
-    netbootLoader
-  ];
-  CPATH = with besspin; "${riscv-libkeyutils}/include:${riscv-libpam}/include";
-
-  passthru = { inherit besspin; };
+  FETT_GFE_SCRIPT_DIR = "${besspin.testingScripts}/scripts";
+  FETT_NETBOOT = besspin.netbootLoader;
+  FETT_GFE_DEBIAN_FPGA = besspin.debianImage;
+  FETT_GFE_DEBIAN_QEMU = besspin.testgenDebianImageQemu;
+  FETT_GFE_FREEBSD_FPGA = besspin.freebsdImage;
+  FETT_GFE_FREEBSD_QEMU = besspin.freebsdImageQemu;
+  FETT_GFE_BUSYBOX_FPGA = besspin.busyboxImage;
+  FETT_GFE_BUSYBOX_QEMU = besspin.busyboxImageQemu;
 }
