f74f9f5d64bcfaa366a3936137cbd74feb837d4f
Isaiah Mindich <isaiah@galois.com>
2020-06-12 12:18:38 -0700

    Build zlib with clang

--- nix/misc/riscv-zlib.nix
+++ nix/misc/riscv-zlib.nix
@@ -1,18 +1,24 @@
-{ stdenv
+{ clangStdenv
+, lib
 , overrideCC
-, riscv-gcc
+, riscv-clang
+, riscv-llvm
+, riscv-linker
 , fetchFromGitHub
-, crossPrefix ? "riscv64-unknown-linux-gnu" 
+, crossPrefix ? "riscv64-unknown-linux-gnu"
+, sysroot
+, useLLD ? false
 }:
 
 let
-  stdenvRiscv = overrideCC stdenv riscv-gcc;
+  stdenvRiscv = overrideCC clangStdenv riscv-clang;
 
-  ABIFlags = "-march=rv64imafdc -mabi=lp64d -fPIC";  
-  CC="${crossPrefix}-gcc ${ABIFlags}";
-  LD="${crossPrefix}-gcc ${ABIFlags}";
-  AR="${crossPrefix}-ar";
-  RANLIB="${crossPrefix}-ranlib";
+  ABIFlags = "-march=rv64imafdc -mabi=lp64d -fPIC";
+  ldSelect = if useLLD then " -fuse-ld=lld" else " --gcc-toolchain=${riscv-linker}";
+  CC="clang -target ${crossPrefix} ${ABIFlags} --sysroot=${sysroot} -Wall -lrt -fPIC -mno-relax ${ldSelect}";
+  LD=CC;
+  AR="llvm-ar";
+  RANLIB="llvm-ranlib";
 
 in stdenvRiscv.mkDerivation rec {
     pname = "${crossPrefix}-riscv-zlib";
@@ -24,6 +30,11 @@ in stdenvRiscv.mkDerivation rec {
       rev = "cacf7f1d4e3d44d871b605da3b647f07d718623f";
       sha256 = "037v8a9cxpd8mn40bjd9q6pxmhznyqpg7biagkrxmkmm91mgm5lg";
     };
-    
+
+    buildInputs = [ riscv-llvm riscv-linker ];
+
+    dontFixup = true;
+    dontPatch = true;
+
     inherit CC LD AR RANLIB;
   }
