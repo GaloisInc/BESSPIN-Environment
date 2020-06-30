95e1fa853e79c3b9e9302702b86ca0a9b7a97887
Isaiah Mindich <isaiah@galois.com>
2020-06-30 14:31:51 -0700

    Support building Debian disk images

--- nix/gfe/debian-initramfs.nix
+++ nix/gfe/debian-initramfs.nix
@@ -1,13 +1,15 @@
-{ stdenv, qemu, chainloaderImage, debianStage1VirtualDisk
+{ stdenv, lib, qemu, chainloaderImage, debianStage1VirtualDisk
 , targetSsh ? null 
-, extraSetup ? null }:
+, extraSetup ? null
+, buildDiskImage ? false }:
 
 let
   extraSetupCp = if extraSetup != null then "cp ${extraSetup} virtfs/extra-setup.sh" else "";
   extraSetupArg = if extraSetup != null then "besspin.extra_setup=/mnt/extra-setup.sh" else "";
+  diskImageArg = lib.optionalString buildDiskImage "besspin.rootfs_image=y";
   sshSetup = if targetSsh != null then "mkdir virtfs/ssh-riscv && cp -rf ${targetSsh}/* virtfs/ssh-riscv" else "";
 in stdenv.mkDerivation rec {
-  name = "debian.cpio.gz";
+  name = if buildDiskImage then "debian-rootfs.img" else "debian.cpio.gz";
 
   buildInputs = [ qemu ];
 
@@ -25,14 +27,15 @@ in stdenv.mkDerivation rec {
     qemu-system-riscv64 \
       -nographic -machine virt -m 2G \
       -kernel ${chainloaderImage} \
-      -append "console=ttyS0 besspin.set_clock=@$(date -d '2020-01-01' +%s) ${extraSetupArg}" \
+      -append "console=ttyS0 besspin.set_clock=@$(date -d '2020-01-01' +%s) ${extraSetupArg} ${diskImageArg}" \
       -fsdev local,id=virtfs,path=$(pwd)/virtfs,security_model=mapped-file \
       -device virtio-9p-device,fsdev=virtfs,mount_tag=virtfs
-
-    gzip -c --best <virtfs/debian-initramfs.cpio >debian-initramfs.cpio.gz
   '';
 
-  installPhase = ''
+  installPhase = if buildDiskImage then ''
+    cp virtfs/debian-rootfs.img $out
+  '' else ''
+    gzip -c --best <virtfs/debian-initramfs.cpio >debian-initramfs.cpio.gz
     cp debian-initramfs.cpio.gz $out
   '';
 }
