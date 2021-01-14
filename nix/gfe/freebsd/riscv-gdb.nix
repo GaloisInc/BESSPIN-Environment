fa937fce85c1eac7feed3e276234feb6e19dcb54
rtadros <rtadros@galois.com>
2021-01-14 09:32:39 -0800

    no need for the things copied from openssh installation

--- nix/gfe/freebsd/riscv-gdb.nix
+++ nix/gfe/freebsd/riscv-gdb.nix
@@ -1,6 +1,5 @@
 { clangStdenv
 , riscv-clang, riscv-llvm, riscv-lld
-, autoconf, automake, autoreconfHook
 , overrideCC
 , sysroot
 , crossPrefix ? "riscv64-unknown-freebsd12.1" 
@@ -29,7 +28,7 @@ in stdenvRiscv.mkDerivation {
     sha256="0a4arq0vaf9fhwxchv5p8ll8lfwbapw905zf33n0w3a4jb2nw9mj";
   };
 
-  nativeBuildInputs = [autoconf automake autoreconfHook riscv-llvm texinfo bison flex python3 perl gawk];
+  nativeBuildInputs = [riscv-llvm texinfo bison flex python3 perl gawk];
 
   configureFlags = [
       "--host=${crossPrefix}"
