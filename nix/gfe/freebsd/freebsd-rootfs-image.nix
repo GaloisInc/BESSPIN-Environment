b104dbc1ce622d19b26298d0ddbc842533c65271
rtadros <rtadros@galois.com>
2021-02-01 09:13:19 -0800

    create the directory + use relative path

--- nix/gfe/freebsd/freebsd-rootfs-image.nix
+++ nix/gfe/freebsd/freebsd-rootfs-image.nix
@@ -79,7 +79,8 @@ in stdenv.mkDerivation rec {
 
       # ./lib/libz.so.1 type=file uname=root gname=wheel mode=0755
   '' + lib.optionalString (targetGdb != null) ''
-      install ${targetGdb} /usr/local/sbin/gdb
+      mkdir -p ./usr/local/sbin
+      install ${targetGdb} ./usr/local/sbin/gdb
   '' + ''
     makefs -N etc -D -f 10000 -o version=2 -s $imageSize riscv.img METALOG
   '';
