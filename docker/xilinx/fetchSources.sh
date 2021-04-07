b10dd301ce104488f9389b68bebe04c4a1d33766
rtadros125 <rtadros@galois.com>
2021-04-07 16:20:40 +0000

    add initial implementation of the xilinx images

---
+++ docker/xilinx/fetchSources.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/env bash
+
+set -e
+
+if [ -z $API_KEY ]; then
+    echo "API_KEY is unset!"
+    exit 1
+fi
+
+function pullArtifactory() {
+  curl -H 'X-JFrog-Art-Api:'$API_KEY \
+    -O "https://artifactory.galois.com/artifactory/$1/$2"
+}
+
+for xFile in \
+        install_config_vivado.txt  \
+        Xilinx_Vivado_Lab_Lin_2019.1_0524_1430.tar.gz \
+        install_config_hw.txt \
+        Xilinx_HW_Server_Lin_2019.1_0524_1430.tar.gz ; do
+    if [ ! -f $xFile ]; then
+        pullArtifactory galwegians_generic-local $xFile
+    fi
+done
+
+for xFile in \
+        gfe_ci_id_ed25519.pub  \
+        gfe_ci_id_ed25519 ; do
+    if [ ! -f $xFile ]; then
+        pullArtifactory besspin_generic-nix $xFile
+        mv $xFile ${xFile#"gfe_ci_"}
+    fi
+done
