cc0fcacfd835caf43dcba65258ade9b19cd1e936
Ramy Tadros <rtadros@galois.com>
2021-04-16 13:29:01 -0700

    implement fetchResources in python

--- docker/build-docker.py
+++ docker/build-docker.py
@@ -5,12 +5,15 @@ Utility to build/publish the docker containers in this directory.
 Alternatively, you can always look at the READMEs and execute the commands manually.
 """
 
-import os, sys, subprocess 
-import logging, argparse
+import os, sys, subprocess, traceback 
+import logging, argparse, json
 
 PUBLIC_PATH = "galoisinc/besspin:"
-PRIVATE_PATH = "artifactory.galois.com:5008/" 
+PRIVATE_DOCKER_PATH = "artifactory.galois.com:5008/" 
+PRIVATE_RESOURCES_PATH = "https://artifactory.galois.com/artifactory/besspin_generic-nix/docker-private-resources"
+TEST_FILE = "test.json"
 
+# Note that the order of these images preserves the inter-dependencies.
 IMAGES_DATA = {
     # GFE Image
     "gfe" : {
@@ -26,7 +29,7 @@ IMAGES_DATA = {
     "tool-suite" : {
         "perm" : "public",
         "resources" : [
-            "galoisCredentialsNetrc.txt "
+            "galoisCredentialsNetrc.txt"
         ],
         "secrets" : [
             "id=galoisCredentialsNetrc,src=./galoisCredentialsNetrc.txt"
@@ -70,7 +73,21 @@ IMAGES_DATA = {
         ]
     }
 
-}
+} #IMAGES_DATA
+
+def error(msg):
+    logging.error(msg)
+    exit(1)
+
+def curlArtifactory(resource, path):
+    try:
+        subprocess.run(["curl", "-H", f"X-JFrog-Art-Api:{os.environ['API_KEY']}",
+                        "-o", os.path.join(path,resource), 
+                        f"{PRIVATE_RESOURCES_PATH}/{resource}"],
+                        check=True)
+    except Exception as exc:
+        traceback.print_exc()
+        error(f"Failed to fetch <{resource}>.")
 
 def main(xArgs):
     dockerDir = os.path.abspath(os.path.dirname(__file__))
@@ -90,19 +107,63 @@ def main(xArgs):
     logging.getLogger().addHandler(console)
 
     # Determine the mode
-    if ((not xArgs.build) and (not xArgs.push) and (not xArgs.fetchResources)):
+    if (not any([xArgs.build, xArgs.push, xArgs.fetchResources])):
         logging.warning("Nothing to do.")
         exit(0)
     logging.debug(f"<fetchResources={xArgs.fetchResources}>, <Build={xArgs.build}>, <Push={xArgs.push}>.")
 
     # Choose the image(s)
+    if (xArgs.selectImages):
+        for image in xArgs.selectImages:
+            if (image not in IMAGES_DATA):
+                error(f"Unknown <{image}>. Please choose from [{','.join(list(IMAGES_DATA.keys()))}]")
+        images = xArgs.selectImages
+    else:
+        images = list(IMAGES_DATA.keys())
+    logging.debug(f"Selected Images: [{','.join(images)}].")
 
-    # For each image
+    goodApikey = False
+    for image in images:
+        logging.info ('\n' + '-'*30 + f">>> <{image}> <<<" + '-'*30)
+        data = IMAGES_DATA[image]
+        path = os.path.join(dockerDir,image)
+
         # Fetch the resources
+        if (xArgs.fetchResources or xArgs.build):
+            logging.debug(f"Fetching resources for <{image}>...")
+            resourcesToFetch = []
+            if ("resources" in data):
+                for resource in data["resources"]:
+                    file = os.path.join(path,resource)
+                    if (os.path.isfile(file)):
+                        logging.debug(f"<{file}> found. Skipping the fetch...")
+                    else:
+                        resourcesToFetch.append(resource)
+            if ((len(resourcesToFetch)>0) and (not goodApikey)): # check for the API key to exit early if not found
+                if ("API_KEY" not in os.environ):
+                    error(f"<API_KEY> is unset! Cannot fetch resources for <{image}>.")
+                if (os.path.isfile(f"/tmp/{TEST_FILE}")):
+                    os.remove(f"/tmp/{TEST_FILE}")
+                # Artifactory returns a json with bad status for bad API keys, so let's fetch the test file
+                curlArtifactory(TEST_FILE,"/tmp")
+                with open(f"/tmp/{TEST_FILE}","r") as f:
+                    testDict = json.load(f)
+                if (("text" in testDict) and (testDict["text"] == "OK!")):
+                    goodApikey = True
+                else:
+                    logging.debug(f"Curl output: {testDict}")
+                    error(f"Invalid <test.json>. Please verify your <API_KEY>.")
 
+            for resource in resourcesToFetch:
+                logging.debug(f"Fetching <{resource}>...")
+                curlArtifactory(resource,path)
+            logging.debug(f"Done fetching resources for <{image}>.")
 
+        
         # Build image
+        # Pre commands
         # DOCKER_BUILDKIT=1, progress=plain, --ssh default, --network=host
+        # Post commands
 
         # Push image
 
