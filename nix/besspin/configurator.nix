b95cf814e830a540aa19e2d7f8cada47c31d0e3f
Brett Boston <boston@galois.com>
2021-04-22 16:44:53 -0700

    Update nix to use github links for ported repos

--- nix/besspin/configurator.nix
+++ nix/besspin/configurator.nix
@@ -4,7 +4,7 @@ srcOnly {
   name = "besspin-configurator";
 
   src = fetchGit2 {
-    url = "git@gitlab-ext.galois.com:ssith/besspin-ui.git";
+    url = "git@github.com:GaloisInc/BESSPIN-UI.git";
     rev = "559985b1e1cec1915e1d8356dbb60dfb4d28f151";
     ref = "master";
   };
