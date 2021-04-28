8ddaa80c4cd83c0869cbe230978c01b6d08a1054
rtadros <rtadros@galois.com>
2021-04-28 13:46:31 -0700

    point to arch-extract on github

--- nix/besspin/arch-extract-src.nix
+++ nix/besspin/arch-extract-src.nix
@@ -1,6 +1,6 @@
 { fetchGit2 }:
 
 fetchGit2 {
-  url = "git@gitlab-ext.galois.com:ssith/arch-extract.git";
+  url = "git@github.com:GaloisInc/BESSPIN-arch-extract.git";
   rev = "71008b9755f099986ed6a69736c2a676b0c404ec";
 }
