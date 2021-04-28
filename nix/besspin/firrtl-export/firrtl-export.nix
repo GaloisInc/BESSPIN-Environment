9df78e193faf7fed9ad31eca529d8548c589cfd1
rtadros <rtadros@galois.com>
2021-04-28 15:47:43 -0700

    move over firrtl-export from gitlab

--- nix/besspin/firrtl-export/firrtl-export.nix
+++ nix/besspin/firrtl-export/firrtl-export.nix
@@ -4,10 +4,7 @@ stdenv.mkDerivation rec {
   name = "firrtl-export";
   version = "0.1.0-SNAPSHOT";
 
-  src = fetchGit2 {
-    url = "git@gitlab-ext.galois.com:ssith/firrtl-export.git";
-    rev = "25f7f641e0db1f8bd184fd91ee0d66377d598b4f";
-  };
+  src = "./";
 
   buildInputs = [
     (scalaEnv.withPackages (ps: with ps; [
