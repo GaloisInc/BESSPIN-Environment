cdaf7a8660c7afa98f64c8eee5bb72da3eae8112
rtadros <rtadros@galois.com>
2021-04-29 15:01:24 -0700

    change the source of riscv-timing-tests to point to GaloisInc github

--- nix/besspin/riscv-timing-tests-src.nix
+++ nix/besspin/riscv-timing-tests-src.nix
@@ -1,6 +1,6 @@
 { fetchGit2 }:
 
 fetchGit2 {
-  url = "git@gitlab-ext.galois.com:ssith/riscv-timing-tests.git";
+  url = "git@github.com:GaloisInc/BESSPIN-riscv-timing-tests.git";
   rev = "8b4042adaa1dde7b00373ab391bb67b01591322c";
 }
